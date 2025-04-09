4a47ac874474c200a32ba8d7c148bc0b428ea138
user <keldibekovyahyobek7@gmail.com>
2025-04-09 21:10:41 +0500

    commit

--- lib/bindings/auth_binding.dart
+++ lib/bindings/auth_binding.dart
@@ -1,3 +1,4 @@
+import 'package:LinCor/controllers/splash_controller.dart';
 import 'package:get/get.dart';
 import '../controllers/auth_controller.dart';
 import '../controllers/onboarding_controller.dart';
@@ -5,6 +6,7 @@ import '../controllers/onboarding_controller.dart';
 class AuthBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<SplashController>(() => SplashController());
     Get.lazyPut<OnboardingController>(() => OnboardingController());
     Get.lazyPut<AuthController>(() => AuthController());
   }
--- lib/pages/mani/splash_video.dart
+++ lib/pages/mani/splash_video.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:LinCor/controllers/splash_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:video_player/video_player.dart';
@@ -12,78 +15,27 @@ class SplashVideoPage extends StatefulWidget {
 }
 
 class _SplashVideoPageState extends State<SplashVideoPage> {
-  late VideoPlayerController _controller;
+  var controller = Get.find<SplashController>();
 
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.asset("assets/videos/gemini_video.mp4")
-      ..initialize().then((_) {
-        setState(() {});
-        _controller.play();
-      });
-
-    _controller.addListener(() async {
-      if (_controller.value.position >=
-          _controller.value.duration - Duration(milliseconds: 500)) {
-        SharedPreferences prefs = await SharedPreferences.getInstance();
-        bool hasSeenOnboarding = prefs.getBool("hasSeenOnboarding") ?? false;
-        bool hasRegistered = prefs.getBool("hasRegistered") ?? false;
-
-        if (!hasSeenOnboarding) {
-          await prefs.setBool("hasSeenOnboarding", true);
-          Get.offAll(() => OnboardingPage());
-        } else if (hasRegistered) {
-          Get.offAll(() => HomePage());
-        } else {
-          Get.offAll(() => RegisterPage());
-        }
-      }
-    });
+    controller.initVideoPlayer();
   }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                if (_controller.value.isPlaying) {
-                  _controller.pause();
-                } else {
-                  _controller.play();
-                }
-              });
-            },
-            child: Center(
-              child: _controller.value.isInitialized
-                  ? AspectRatio(
-                      aspectRatio: _controller.value.aspectRatio,
-                      child: VideoPlayer(_controller),
-                    )
-                  : CircularProgressIndicator(),
-            ),
-          ),
-          Positioned(
-            top: 40,
-            right: 20,
-            child: IconButton(
-              icon: Icon(Icons.close, color: Colors.white, size: 30),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ),
-        ],
+      body: GetBuilder<SplashController>(
+        builder: (_) {
+          return Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: controller.videoPlayerController.value.isInitialized
+                ? VideoPlayer(controller.videoPlayerController)
+                : CircularProgressIndicator(),
+          );
+        }
       ),
-    );
+          );
   }
 }
